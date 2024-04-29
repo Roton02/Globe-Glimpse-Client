2a376a70ca9942a265e931f6156b851ffb9e22c1
Roton02 <rph645102@gmail.com>
2024-04-29 10:42:54 +0600

    ser private route loader

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ContextProvider>
       <HelmetProvider>
-      <RouterProvider fallbackElement={<div className="flex min-h-screen my-auto items-center justify-center my-28"><span className="loading loading-bars loading-xs"></span>
+      <RouterProvider fallbackElement={<div className="flex min-h-screen my-auto items-center justify-center"><span className="loading loading-bars loading-xs"></span>
 <span className="loading loading-bars loading-lg"></span>
 <span className="loading loading-bars loading-lg"></span>
 <span className="loading loading-bars loading-lg"></span></div>} router={router} />
