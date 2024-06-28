281e65b55cb5ad6d3cd847ea042671c7e2c9297d
Roton02 <rph645102@gmail.com>
2024-06-29 03:57:17 +0600

    resoled mylist page

--- src/pages/MyList/MyList.jsx
+++ src/pages/MyList/MyList.jsx
@@ -1,139 +1,103 @@
-import React, { useContext, useEffect, useMemo, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../ContextProvider/ContextProvider";
 import { FaRegEdit } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 import Swal from "sweetalert2";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
-import { useTable } from "react-table";
-import "./MyList.css"; // Add custom CSS for styling
 
 const MyList = () => {
-    const { user } = useContext(AuthContext);
-    const [userspot, setUserSpot] = useState([]);
+  const { user } = useContext(AuthContext);
+  const [userspot, setUserSpot] = useState([]);
 
-    useEffect(() => {
-        fetch(`https://globeglimpse.vercel.app/addTousristSpot/${user.email}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setUserSpot(data);
-            })
-            .catch(error => {
-                console.error("Error fetching data:", error);
-            });
-    }, [user.email]); // Only re-run the effect if user.email changes
+  useEffect(() => {
+    fetch(`https://globeglimpse.vercel.app/addTousristSpot/${user.email}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUserSpot(data);
+      });
+  }, []);
 
-    const handleDelete = (id) => {
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                fetch(`https://globeglimpse.vercel.app/addTousristSpot/${id}`, {
-                    method: 'DELETE',
-                })
-                .then(res => res.json())
-                .then(data => {
-                    if (data.deletedCount > 0) {
-                        Swal.fire({
-                            title: "Deleted!",
-                            text: "Your file has been deleted.",
-                            icon: "success"
-                        });
-                        setUserSpot(prevState => prevState.filter(item => item._id !== id));
-                    } else {
-                        Swal.fire({
-                            title: "Failed to Delete!",
-                            text: "Could not delete the item.",
-                            icon: "error"
-                        });
-                    }
-                })
-                .catch(error => {
-                    console.error("Error deleting item:", error);
-                });
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`https://globeglimpse.vercel.app/addTousristSpot/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
             }
-        });
-    };
+            const remaining = userspot.filter((cof) => cof._id !== id);
+            setUserSpot(remaining);
+          });
+      }
+    });
+  };
 
-    const data = useMemo(() => userspot, [userspot]);
-
-    const columns = useMemo(
-        () => [
-            { Header: "ID", accessor: (row, i) => i + 1 },
-            { Header: "Tourist", accessor: "Tourist" },
-            { Header: "Time", accessor: "TravelTime" },
-            { Header: "Country", accessor: "countryName" },
-            {
-                Header: "Update",
-                accessor: "update",
-                Cell: ({ row }) => (
-                    <Link to={`/updateTourist/${row.original._id}`}>
-                        <button className="btn btn-sm ml-2 bg-pink-600">
-                            <FaRegEdit />
-                        </button>
-                    </Link>
-                )
-            },
-            {
-                Header: "Delete",
-                accessor: "delete",
-                Cell: ({ row }) => (
-                    <button onClick={() => handleDelete(row.original._id)} className="btn btn-sm ml-2 btn-warning">
-                        <MdDeleteForever />
+  return (
+    <div className="overflow-x-auto md:w-4/5 mx-auto mt-4">
+      <Helmet>
+        <title>My List</title>
+      </Helmet>
+      <div className="bg-green-100  border border-black">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-slate-200 text-white">
+            <tr className="text-black">
+              <th className="px-6 py-3"></th>
+              <th className="px-6 py-3">Tourist</th>
+              <th className="px-6 py-3">Season</th>
+              <th className="px-6 py-3">Country</th>
+              <th className="px-6 py-3">Update</th>
+              <th className="px-6 py-3">Delete</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {userspot.map((spot, index) => (
+              <tr key={spot._id} className="text-left">
+                <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{spot.Tourist}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {spot.seasonality}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {spot.countryName}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <Link to={`/updateTourist/${spot._id}`}>
+                    <button className="btn bg-pink-600 text-white p-2 rounded-md hover:bg-pink-700">
+                      <FaRegEdit />
                     </button>
-                )
-            }
-        ],
-        [handleDelete]
-    );
-
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow
-    } = useTable({ columns, data });
-
-    return (
-        <div className="">
-            <Helmet>
-                <title>My List</title>
-            </Helmet>
-            <div className="table-container">
-                <table {...getTableProps()}>
-                    <thead>
-                        {headerGroups.map(headerGroup => (
-                            <tr {...headerGroup.getHeaderGroupProps()}>
-                                {headerGroup.headers.map(column => (
-                                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                                ))}
-                            </tr>
-                        ))}
-                    </thead>
-                    <tbody {...getTableBodyProps()}>
-                        {rows.map(row => {
-                            prepareRow(row);
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {row.cells.map(cell => (
-                                        <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                    ))}
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+                  </Link>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <button
+                    onClick={() => handleDelete(spot._id)}
+                    className="btn bg-red-600 text-white p-2 rounded-md hover:bg-red-700"
+                  >
+                    <MdDeleteForever />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyList;
